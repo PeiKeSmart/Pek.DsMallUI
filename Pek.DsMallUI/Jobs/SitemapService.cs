13b5db321f6bbad121e202f94a0850460b39195c
猿人易 <qcjxberin@outlook.com>
2024-11-29 11:55:29 +0800

    更新 Pek.NCubeUI 包版本并新增 SitemapService 类
    在 `Pek.DsMallUI.csproj` 文件中更新了 `Pek.NCubeUI` 包的版本，从 `4.0.2024.1129-beta0234` 更新到 `4.0.2024.1129-beta0347`。
    
    在 `SitemapService.cs` 文件中：
    - 添加了多个 `using` 语句，包括 `System.ComponentModel`、`NewLife.Cube.Jobs`、`NewLife.Log` 和 `Pek.NCubeUI.Events`。
    - 新增了 `Pek.DsMallUI.Jobs` 命名空间。
    - 新增了 `SitemapArgument` 类，作为生成 sitemap 作业的参数类。
    - 新增了 `SitemapService` 类，继承自 `CubeJobBase<SitemapArgument>`，用于定时生成网站的 sitemap。
      - 该类包含两个私有字段 `_tracer` 和 `_eventPublisher`，分别用于追踪和事件发布。
      - 在构造函数中初始化了这两个字段。
      - 重写了 `OnExecute` 方法，执行生成 sitemap 的作业逻辑，并发布 `SiteMapEvent` 事件。
      - 注释掉了一些与服务器端协定密钥相关的代码。

---
+++ Pek.DsMallUI/Jobs/SitemapService.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+
+using NewLife.Cube.Jobs;
+using NewLife.Log;
+
+using Pek.NCubeUI.Events;
+
+namespace Pek.DsMallUI.Jobs;
+
+/// <summary>
+/// 生成sitemap作业参数
+/// </summary>
+public class SitemapArgument
+{
+
+}
+
+/// <summary>生成sitemap服务</summary>
+[DisplayName("生成sitemap")]
+[Description("定时生成网站Sitemap")]
+[CronJob("Sitemap", "20 1 * * * ? *", Enable = true)]
+public class SitemapService : CubeJobBase<SitemapArgument>
+{
+    private readonly ITracer _tracer;
+    private readonly IEventPublisher _eventPublisher;
+
+    /// <summary>实例化生成sitemap服务</summary>
+    /// <param name="tracer"></param>
+    /// <param name="eventPublisher"></param>
+    public SitemapService(ITracer tracer, IEventPublisher eventPublisher)
+    {
+        _tracer = tracer;
+        _eventPublisher = eventPublisher;
+    }
+
+    /// <summary>执行作业</summary>
+    /// <param name="argument"></param>
+    /// <returns></returns>
+    protected override Task<String> OnExecute(SitemapArgument argument)
+    {
+        using var span = _tracer?.NewSpan("Sitemap", argument);
+
+        _eventPublisher.Publish(new SiteMapEvent());
+
+        //var Token = DHSetting.Current.ServerToken;  // 获取与服务器端协定的密钥
+        //var TimeStamp = UnixTime.ToTimestamp();
+        //var Nonce = Pek.Helpers.Randoms.RandomString(6);
+        //var Sign = CheckSignature.Create(TimeStamp, Nonce, Token);
+
+        //await $"{Setting.Current.CurDomainUrl.Trim('/')}/Site/SiteMap"
+        //    .WithHeader("Signature", Sign)
+        //    .WithHeader("Nonce", Nonce)
+        //    .WithHeader("TimeStamp", TimeStamp)
+        //    .GetAsync();
+
+        return Task.FromResult("OK");
+    }
+}
