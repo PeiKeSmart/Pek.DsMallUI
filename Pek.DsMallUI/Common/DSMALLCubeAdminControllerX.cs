a036125ec65c94f5866d668773e407989aa987cb
猿人易 <qcjxberin@outlook.com>
2024-11-29 10:44:45 +0800

    添加配置文件和项目结构，优化构建和发布流程
    在 `.editorconfig` 文件中添加了多个配置项，包括缩进样式、编码格式、换行符、C# 和 VB 的代码风格设置等。
    
    在 `.gitignore` 文件中添加了多个忽略规则，主要针对 Visual Studio 生成的文件和目录。
    
    添加了 `global.json` 文件，指定了 .NET SDK 的版本和相关设置。
    
    添加了 `LICENSE` 文件，使用 MIT 许可证，并包含了版权声明。
    
    添加了 `NuGet.config` 文件，配置了 NuGet 包源。
    
    添加了 `Pek.DsMallUI.sln` 文件，定义了 Visual Studio 解决方案及其项目结构。
    
    在 `README.md` 文件中添加了项目介绍和特性说明。
    
    添加了 `publish-beta.yml` 文件，定义了 GitHub Actions 工作流，用于在推送到 `main` 分支时构建和发布 beta 版本。
    
    添加了 `publish.yml` 文件，定义了 GitHub Actions 工作流，用于在推送带有 `v*` 标签时构建和发布正式版本。
    
    在 `docker-build.bat` 文件中，添加了一行命令，用于构建 Docker 镜像。
    
    在 `docker.txt` 文件中，添加了多行内容，详细描述了如何在本地测试魔方镜像。
    
    在 `打包命令.txt` 文件中，添加了一个新的打包命令。
    
    在 `Pek.DsMallUI.csproj` 文件中，添加了一个新的项目文件配置。
    
    在 `PekStartup.cs` 文件中，添加了一个新的启动类 `PekStartup`。
    
    在 `DSMALLCubeAdminControllerX.cs` 文件中，添加了一个新的后台控制器基类。
    
    在 `placeholder.txt` 文件中，添加了一个新的内容 `点位`。

---
+++ Pek.DsMallUI/Common/DSMALLCubeAdminControllerX.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NewLife.Collections;
+
+using Pek.NCube;
+
+namespace Pek.DsMallUI;
+
+/// <summary>
+/// 后台控制器基类
+/// </summary>
+public class DSMALLCubeAdminControllerX : PekAdminControllerBaseX
+{
+    /// <summary>
+    /// 消息跳转
+    /// </summary>
+    /// <param name="Message"></param>
+    /// <returns></returns>
+    public IActionResult MessageTip(String Message)
+    {
+        var html = Pool.StringBuilder.Get();
+        html.Append("<script>parent.layer.alert('" + Message + "',{yes:function(index, layero){parent.location.reload();},cancel:function(index, layero){parent.location.reload();}});</script>");
+        return Content(html.Return(true), "text/html");
+    }
+}
