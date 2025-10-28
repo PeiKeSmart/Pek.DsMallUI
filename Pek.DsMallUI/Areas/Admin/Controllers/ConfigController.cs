f1bd4fb97658fcad178798529e998f47a0f61736
猿人易 <qcjxberin@outlook.com>
2025-10-28 16:40:34 +0800

    更新站点设置功能及前端页面优化
    - 调整命名空间，更新为 `HlktechPower.Areas.Admin.Controllers`。
    - 更新 `DHMenu` 注解及方法权限，优化代码结构。
    - 新增多项功能，包括移动端模板支持、SSL 模式配置、多语言动态渲染等。
    - 删除冗余代码和注释，提升代码可读性。
    - 前端页面新增多项表单字段及提示信息，优化用户体验。
    - 增强配置动态更新能力，支持 `SwaggerOption:Enabled` 配置。
    - 格式化代码，统一样式，提升代码质量。
    - 添加日志记录，便于调试和问题排查。

--- Pek.DsMallUI/Areas/Admin/Controllers/ConfigController.cs
+++ Pek.DsMallUI/Areas/Admin/Controllers/ConfigController.cs
@@ -1,5 +1,3 @@
-﻿using System.ComponentModel;
-
 using DH.Core.Domain.Localization;
 using DH.Entity;
 
@@ -7,33 +5,38 @@ using Microsoft.AspNetCore.Mvc;
 
 using NewLife;
 using NewLife.Common;
-
+using NewLife.Log;
+using Pek;
+using Pek.Configs;
+using Pek.DsMallUI;
 using Pek.Models;
 using Pek.NCubeUI.Areas.Admin;
 using Pek.NCubeUI.Common;
 
+using System.ComponentModel;
+
 using XCode.Membership;
 
-namespace Pek.DsMallUI.Areas.Admin.Controllers;
+namespace HlktechPower.Areas.Admin.Controllers;
 
 /// <summary>站点设置</summary>
 [DisplayName("站点设置")]
 [Description("用于站点的站点设置、防灌水设置等基础设置")]
 [AdminArea]
-[DHMenu(100, ParentMenuName = "Settings", ParentMenuDisplayName = "设置", ParentMenuUrl = "~/{area}/Config", ParentMenuOrder = 95, ParentVisible = true, CurrentMenuUrl = "~/{area}/Config", CurrentMenuName = "ConfigSetting", CurrentIcon = "&#xe6e0;", LastUpdate = "20251014")]
+[DHMenu(100, ParentMenuName = "Settings", ParentMenuDisplayName = "设置", ParentMenuUrl = "~/{area}/Config", ParentMenuOrder = 95, ParentVisible = true, CurrentMenuUrl = "~/{area}/Config", CurrentMenuName = "ConfigSetting", CurrentIcon = "&#xe6e0;", LastUpdate = "20250527")]
 public class ConfigController : PekCubeAdminControllerX
 {
+
     /// <summary>
     /// 站点设置
     /// </summary>
     /// <returns></returns>
-    [EntityAuthorize(PermissionFlags.Update)]
+    [EntityAuthorize(PermissionFlags.Detail)]
     [DisplayName("站点设置")]
     public IActionResult Index()
     {
         ViewBag.LanguageList = Language.FindByStatus().OrderBy(e => e.DisplayOrder);
         ViewBag.Model = SiteInfo.FindDefault();
-
         return View();
     }
 
@@ -44,13 +47,15 @@ public class ConfigController : PekCubeAdminControllerX
     [EntityAuthorize(PermissionFlags.Update)]
     [HttpPost]
     [DisplayName("站点设置")]
-    public IActionResult UpdateBase(String curDomainUrl, Int32 isEnableLanguage, String Registration, Int32 isAllowSwagger, Int32 isAllowMarkupMin, Int32 refreshuserperiod, Int32 sessiontimeout, Int32 logoutAll, Int32 useSsoRole, String companyname, String Copyright, Int32 allowLogin, Int32 isAllowUrlSuffix, String SiteName, String SeoDescribe, String SeoKey, String SeoTitle, Int32 seoFriendlyUrlsForLanguagesEnabled, IFormFile site_logowx, IFormFile site_logo, IFormFile site_icon, String WwwRequirement, String SslEnabled, Int32 SiteIsClose, IFormFile memberlogo, IFormFile membersmalllogo, String UrlSuffix, String Statistical, String PageFoot, Int32 PageTitleSeoAdjustment, String CustomerTel, Int32 AutoRegister, Int32 isHtmlStaticDevelopmentMode, Int32 htmlStaticExpireMinutes, String ServerToken, Int32 AllowRequestParams, Int32 EnableOnlineStatistics, Int32 MaxOnlineCount, String BanAccessTime, String BanAccessIP, String AllowAccessIP, Int32 OnlineCountExpire, Int32 UpdateOnlineTimeSpan, Int32 OnlineUserExpire, String PaswordStrength, String StarWeb, String FooterCustomHtml, String HeaderCustomHtml)
+    public IActionResult UpdateBase(String curDomainUrl, Int32 isEnableLanguage, String Registration, Int32 isAllowSwagger, Int32 isAllowMarkupMin, Int32 refreshuserperiod, Int32 sessiontimeout, Int32 logoutAll, Int32 useSsoRole, String companyname, String Copyright, Int32 allowLogin, Int32 isAllowUrlSuffix, String SiteName, String SeoDescribe, String SeoKey, String SeoTitle, Int32 seoFriendlyUrlsForLanguagesEnabled, IFormFile site_logowx, IFormFile site_logo, IFormFile site_icon, String WwwRequirement, String SslEnabled, Int32 SiteIsClose, IFormFile memberlogo, IFormFile membersmalllogo, String UrlSuffix, String Statistical, String PageFoot, Int32 PageTitleSeoAdjustment, String CustomerTel, Int32 AutoRegister, Int32 isHtmlStaticDevelopmentMode, Int32 htmlStaticExpireMinutes, String ServerToken, Int32 AllowRequestParams, Int32 EnableOnlineStatistics, Int32 MaxOnlineCount, String BanAccessTime, String BanAccessIP, String AllowAccessIP, Int32 OnlineCountExpire, Int32 UpdateOnlineTimeSpan, Int32 OnlineUserExpire, String PaswordStrength, String StarWeb, String FooterCustomHtml, String HeaderCustomHtml, Int32 AllowMobileTemp)
     {
         var Model = SiteInfo.FindDefault();
         if (SiteName.IsNullOrWhiteSpace())
         {
             return Prompt(new PromptModel { Message = GetResource("网站名称不可为空") });
         }
+
+        ConfigFileHelper.AddOrUpdateAppSetting("SwaggerOption:Enabled", isAllowSwagger == 1, "Settings/Swagger.json");
 
         Model.SeoTitle = SeoTitle.SafeString().Trim();
         Model.SeoKey = SeoKey.SafeString().Trim();
@@ -92,10 +97,10 @@ public class ConfigController : PekCubeAdminControllerX
                     ex.Registration = (GetRequest($"[{item.Id}].Registration")).SafeString().Trim();
                     ex.SiteCopyright = (GetRequest($"[{item.Id}].SiteCopyright")).SafeString().Trim();
 
-                    if (ex.SiteName.IsNullOrWhiteSpace() && ex.SeoDescribe.IsNullOrWhiteSpace() && ex.SeoKey.IsNullOrWhiteSpace() && ex.SeoTitle.IsNullOrWhiteSpace())
-                    {
-                        continue;
-                    }
+                    //if (ex.SiteName.IsNullOrWhiteSpace() && ex.SeoDescribe.IsNullOrWhiteSpace() && ex.SeoKey.IsNullOrWhiteSpace() && ex.SeoTitle.IsNullOrWhiteSpace())
+                    //{
+                    //    continue;
+                    //}
 
                     ex.LanguageId = item.Id;
                     ex.SiteInfoId = 1;
@@ -128,8 +133,37 @@ public class ConfigController : PekCubeAdminControllerX
         set.HtmlStaticExpireMinutes = htmlStaticExpireMinutes;
         set.PaswordStrength = PaswordStrength;
         set.PageTitleSeoAdjustment = PageTitleSeoAdjustment;
+        set.AllowMobileTemp = AllowMobileTemp == 1;
+        set.MaxOnlineCount = MaxOnlineCount;
+        set.UpdateOnlineTimeSpan = UpdateOnlineTimeSpan;
+        set.BanAccessTime = BanAccessTime;
+        set.BanAccessIP = BanAccessIP;
+        set.AllowAccessIP = AllowAccessIP;
+
+        switch (SslEnabled)
+        {
+            default:
+                set.SslEnabled = 0;
+                set.AllSslEnabled = false;
+                break;
+            case "1":
+                set.SslEnabled = 1;
+                set.AllSslEnabled = false;
+                break;
+            case "2":
+                set.SslEnabled = 2;
+                set.AllSslEnabled = false;
+                break;
+            case "99":
+                set.SslEnabled = 1;
+                set.AllSslEnabled = true;
+                break;
+        }
 
         set.Save();
+
+        PekSysSetting.Current.AllowRequestParams = AllowRequestParams == 1;
+        PekSysSetting.Current.Save();
 
         LocalizationSettings.Current.IsEnable = isEnableLanguage == 1;
         LocalizationSettings.Current.SeoFriendlyUrlsForLanguagesEnabled = seoFriendlyUrlsForLanguagesEnabled == 1;
@@ -137,5 +171,5 @@ public class ConfigController : PekCubeAdminControllerX
 
         return Prompt(new PromptModel { Message = GetResource("保存成功"), IsOk = true, BackUrl = Url.Action("Index") });
     }
-
 }
+
